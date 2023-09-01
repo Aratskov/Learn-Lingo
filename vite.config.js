faaacfbc2c9348dd46488a209d7a8317f248ef49
Andrii <ifromukraine2022@gmail.com>
2023-09-01 12:32:44 +0300

    add homepage

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/learn-lingo/",
-  rollupOptions: {
-      external: ["react", "react-router", "react-router-dom", "react-redux"],
-      output: {
-        globals: {
-          react: "React",
-        },
-      },
-    },
+  // rollupOptions: {
+  //     external: ["react", "react-router", "react-router-dom", "react-redux"],
+  //     output: {
+  //       globals: {
+  //         react: "React",
+  //       },
+  //     },
+  //   },
 })
